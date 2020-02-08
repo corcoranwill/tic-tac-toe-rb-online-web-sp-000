c68762c2b840c809c68f3195794c8a149f5a92b1
Will Corcoran <corcoranwill@gmail.com>
2020-02-08 22:44:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,6 +82,14 @@ end
 def won?(board)
   # returns false for draw?()
   # returns true for win()
+  WIN_COMBINATIONS.detect do |win_combo|
+    position_1 = board[win_combo[0]]
+    position_2 = board[win_combo[1]]
+    position_3 = board[win_combo[2]]
+
+    position_1 == "X" && position_2 == "X" && position_3 == "X" ||
+    position_1 == "O" && position_2 == "O" && position_3 == "O"
+  end
 end
 
 # Comment
