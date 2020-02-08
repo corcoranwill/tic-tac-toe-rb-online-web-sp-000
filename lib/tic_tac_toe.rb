ededad7397ee63476f7dced538d27b788e1d8f17
Will Corcoran <corcoranwill@gmail.com>
2020-02-08 21:17:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -6,7 +6,7 @@ WIN_COMBINATIONS = [
   [3,4,5], # middle row
   [6,7,8], # bottom row
   [0,3,6], # left column
-  [1,4,7], # middle column  
+  [1,4,7], # middle column
   [2,5,8], # right column
   [0,4,8], # diag 1
   [2,4,6]  # diag 2
