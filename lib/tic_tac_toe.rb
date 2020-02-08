af71a8d629c2e9b38f11212fff21ef43b1d2855b
Will Corcoran <corcoranwill@gmail.com>
2020-02-08 21:31:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -28,37 +28,57 @@ end
 # Comment
 def move(board, position, token)
   # does not allow for a 3rd default argument
-
+  # places the token on the board in the position specified 
 end
 
 # Comment
 def position_taken?(board)
   # return true if a valid token is in a valid position
   # returns false if nil or " "
-  
 end
 
 # Comment
-def display_board(board)
-
-end
-
-# Comment
-def display_board(board)
+def valid_move?(board)
 
 end
 
 # Comment
-def display_board(board)
+def turn(board)
 
 end
 
 # Comment
-def display_board(board)
+def turn_count(board)
 
 end
 
 # Comment
-def display_board(board)
+def current_player(board)
 
 end
+
+# Comment
+def won?(board)
+
+end
+
+# Comment
+def full?(board)
+
+end
+
+# Comment
+def draw?(board)
+
+end
+
+# Comment
+def over?(board)
+
+end
+
+# Comment
+def winner(board)
+
+end
+
