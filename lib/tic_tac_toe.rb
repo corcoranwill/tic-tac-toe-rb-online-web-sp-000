f789d46ffea3460dfb535653cdaf159b1a0d2122
Will Corcoran <corcoranwill@gmail.com>
2020-02-09 01:12:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,15 @@
+def play()
+# until the game is over
+#   take turns
+# end
+#  
+# if the game was won
+#   congratulate the winner
+# else if the game was a draw
+#   tell the players it has been a draw
+# end
+end
+
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
   [0,1,2], # top row
